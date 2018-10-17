0f056335bc72e31760414403e048b442af125df7
anna <anytaromantsova@gmail.com>
2018-10-17 18:18:48 +0300

    upload commit

---
+++ app/src/main/java/itstap/edu/hwradio/list/Shoutcast.java
@@ -0,0 +1,30 @@
+package itstap.edu.hwradio.list;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class Shoutcast {
+    private String name;
+
+    @SerializedName("stream")
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+}
